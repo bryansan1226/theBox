4a63ba161240fc0692a79f85c9651b170dc33619
bryansan1226 <70733299+bryansan1226@users.noreply.github.com>
2023-11-03 14:31:18 -0400

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const backendUrl =
   process.env.NODE_ENV === "production"
-    ? `https://the-box-social-media-bryansan26.herokuapp.com`
+    ? `https://the-box-social-media-bryansan26.herokuapp.com/`
     : "http://localhost:8080/";
 export default backendUrl;
