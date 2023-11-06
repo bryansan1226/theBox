41b1fddc32863cd30add834a2f29978b2c4c5493
bryansan1226 <70733299+bryansan1226@users.noreply.github.com>
2023-11-06 17:16:42 -0500

    .

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 //import reportWebVitals from "./reportWebVitals";
-console.log("I am in index.js");
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
