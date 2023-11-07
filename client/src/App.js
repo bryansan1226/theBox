6da585d6b16b95a0e844cdb475ad378d22a757cb
bryansan1226 <70733299+bryansan1226@users.noreply.github.com>
2023-11-07 14:48:32 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+/*import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import SearchResults from "./pages/SearchResults";
@@ -20,6 +20,34 @@ function App() {
         <Route path="/conversation" element={<Conversation />} />
       </Routes>
     </BrowserRouter>
+    
+  );
+}
+
+export default App;*/
+
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import SearchResults from "./pages/SearchResults";
+import Messages from "./pages/Messages";
+import Conversation from "./pages/Conversation";
+import NotificationsPage from "./pages/NotificationsPage";
+import React from "react";
+
+function App() {
+  console.log("In app.js");
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/searchResults" element={<SearchResults />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/conversation" element={<Conversation />} />
+      </Routes>
+    </Router>
   );
 }
 
